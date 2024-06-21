f59c5c5d48a3b9ba7d4d1a33d60fb26964915183
MohammadNasser-CS <abualnasser198@gmail.com>
2024-06-21 09:34:32 +0200

    coninue linking

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housing_project/Utils/app_color.dart';
 import 'package:housing_project/Utils/routes/app_router.dart';
 import 'package:housing_project/Utils/routes/app_routes.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:housing_project/controllers/auth_cubit/auth_cubit.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  MyApp({super.key});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -20,71 +17,56 @@ class MyApp extends StatelessWidget {
     return ValueListenableBuilder(
         valueListenable: ValueNotifier(const Locale('ar')),
         builder: (context, locale, _) {
-          return BlocProvider(
-            create: (context) => AuthCubit()..checkAuthStatus(),
-            child: BlocListener<AuthCubit, AuthState>(
-              listener: (context, state) {
-                if (state is AuthInitial) {
-                  navigatorKey.currentState
-                      ?.pushReplacementNamed(AppRoutes.loginPage);
-                } else if (state is AuthSuccess) {
-                  navigatorKey.currentState
-                      ?.pushReplacementNamed(AppRoutes.home);
-                }
-              },
-              child: MaterialApp(
-                title: 'Housing System',
-                debugShowCheckedModeBanner: false,
-                theme: ThemeData(
-                  primaryColor: AppColor.white,
-                  scaffoldBackgroundColor: AppColor.grey1,
-                  appBarTheme:
-                      const AppBarTheme(backgroundColor: AppColor.white),
-                  useMaterial3: true,
-                  inputDecorationTheme: InputDecorationTheme(
-                    fillColor: AppColor.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(color: AppColor.grey4),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(color: AppColor.grey4),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(color: AppColor.grey4),
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(color: AppColor.grey4),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: const BorderSide(color: AppColor.red),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: const BorderSide(color: AppColor.red),
-                    ),
-                  ),
+          return MaterialApp(
+            title: 'Housing System',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primaryColor: AppColor.white,
+              scaffoldBackgroundColor: AppColor.grey1,
+              appBarTheme: const AppBarTheme(backgroundColor: AppColor.white),
+              useMaterial3: true,
+              inputDecorationTheme: InputDecorationTheme(
+                fillColor: AppColor.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: AppColor.grey4),
                 ),
-                initialRoute: AppRoutes.loginPage,
-                onGenerateRoute: AppRouter.generateRoute,
-                localizationsDelegates: const [
-                  // S.delegate,
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                  GlobalCupertinoLocalizations.delegate,
-                ],
-                locale: locale,
-                supportedLocales: const [
-                  Locale("en"), // English
-                  Locale("ar"), // Arabic
-                ],
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: AppColor.grey4),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: AppColor.grey4),
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: AppColor.grey4),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: const BorderSide(color: AppColor.red),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: const BorderSide(color: AppColor.red),
+                ),
               ),
             ),
+            initialRoute: AppRoutes.loginPage,
+            onGenerateRoute: AppRouter.generateRoute,
+            localizationsDelegates: const [
+              // S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            locale: locale,
+            supportedLocales: const [
+              Locale("en"), // English
+              Locale("ar"), // Arabic
+            ],
           );
         });
   }
